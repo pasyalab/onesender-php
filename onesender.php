af5896ac8babe1431ac6a34fb735f11c56c35c94
pasyalab <webstoryinc@gmail.com>
2023-12-05 22:39:13 +0700

    Update onesender.php
    Add filter

--- onesender.php
+++ onesender.php
@@ -1,92 +1,48 @@
 <?php
 
-/**
- * Class OneSender
- * 
- * A class for sending messages and media through a messaging API.
- */
 class OneSender {
 
-    /** @var OneSender|null $instance */
     public static $instance;
-
-    /**
-     * Get an instance of the OneSender class.
-     * 
-     * @param string $url The API URL.
-     * @param string $key The API key.
-     * @param string $countryCode The country code.
-     * @param bool $validateUrl Whether to validate URLs.
-     * @return OneSender The OneSender instance.
-     */
-    public static function instance($url = '', $key = '',  $countryCode = '62', $validateUrl = false): OneSender {
+    
+    public static function instance($url = '', $key = '',  $countryCode = '62', $validateUrl = false, string $app = 'api'): OneSender {
         if (!self::$instance) {
             self::$instance = new self(
                 $url, 
                 $key, 
                 $countryCode, 
-                $validateUrl
+                $validateUrl,
+                $app
             );
         }
 
         return self::$instance;
     }
 
-    /**
-     * Get an instance of the OneSender class.
-     * 
-     * @param string $url The API URL.
-     * @param string $key The API key.
-     * @param string $countryCode The country code.
-     * @param bool $validateUrl Whether to validate URLs.
-     * @return OneSender The OneSender instance.
-     */
-    public static function setup($url = '', $key = '',  $countryCode = '62', $validateUrl = false): OneSender {
-        return self::instance($url, $key,  $countryCode, $validateUrl);
+    public static function setup($url = '', $key = '',  $countryCode = '62', $validateUrl = false, string $app = 'api'): OneSender {
+        return self::instance($url, $key,  $countryCode, $validateUrl, $app);
     }
 
-    /** @var string $countryCode The default country code. */
+    const VERSION = '1.1.0';
+
     protected string $countryCode = '62';
-
-    /** @var string $apiUrl The API URL. */
     protected string $apiUrl;
-
-    /** @var string $apiKey The API key. */
     protected string $apiKey;
-
-    /** @var bool $validateUrl Whether to validate URLs. */
+    protected string $app;
     protected bool $validateUrl;
-
-    /** @var array $invalidMessages Array to store invalid messages. */
     protected array $invalidMessages = [];
 
-    /** @var array $messages Array to store messages. */
+    protected array $filters = [];
+
     protected array $messages = [];
 
-    /**
-     * OneSender constructor.
-     * 
-     * @param string $url The API URL.
-     * @param string $key The API key.
-     * @param string $countryCode The country code.
-     * @param bool $validateUrl Whether to validate URLs.
-     */
-    public function __construct($url = '', $key = '', $countryCode = '62', $validateUrl = false) {
+    public function __construct($url = '', $key = '', $countryCode = '62', $validateUrl = false, string $app = 'api') {
         $this->apiUrl = $url;
         $this->apiKey = $key;
         $this->countryCode = $countryCode;
         $this->validateUrl = $validateUrl;
+        $this->app = $app;
     }
 
-    /**
-     * Send a text message.
-     * 
-     * @param string $phone The phone number.
-     * @param string $text The text of the message.
-     * @param bool $unique Whether to make the message unique.
-     * @param bool $aggregate Whether to aggregate messages.
-     * @return mixed The result of the message sending.
-     */
     public function sendText(string $phone = '', string $text = '', $unique = false, $aggregate = false): mixed {
         if (empty($text) || empty($phone)) {
             return [false, 'Text and phone number are required'];
@@ -101,6 +57,7 @@ class OneSender {
 
         $messages = $this->map($phones, function($phone) use ($text, $unique) {
             $message = [
+                'app' => $this->app,
                 'type' => 'text',
                 'to' => $phone['number'],
                 'recipient_type' => $phone['type'],
@@ -109,8 +66,11 @@ class OneSender {
                 ],
             ];
 
+            $tag = $this->randomTag();
+            $tag = $this->applyFilters('message_tag', $tag);
+
             if ($unique) {
-                $message['tag'] = $this->randomTag();
+                $message['tag'] = $tag;
                 $message['unique'] = true;
             }
 
@@ -125,48 +85,15 @@ class OneSender {
         return $this->deliver($messages);
     }
 
-    /**
-     * Send an image message.
-     * 
-     * @param string $phone The phone number.
-     * @param string $url The URL of the image.
-     * @param string $caption The caption for the image.
-     * @param bool $unique Whether to make the message unique.
-     * @param bool $aggregate Whether to aggregate messages.
-     * @return mixed The result of the message sending.
-     */
     public function sendImage(string $phone = '', string $url = '', string $caption = '', $unique = false, $aggregate = false): mixed  {
-        return $this->sendMedia('image', $phone, $url, $caption, '', $unique, $aggregate);
+        return $this->sendMedia('image', $phone, $url, $caption, $unique, $aggregate);
     }
 
-    /**
-     * Send a document message.
-     * 
-     * @param string $phone The phone number.
-     * @param string $url The URL of the document.
-     * @param string $caption The caption for the document.
-     * @param string $filename The filename for the media.
-     * @param bool $unique Whether to make the message unique.
-     * @param bool $aggregate Whether to aggregate messages.
-     * @return mixed The result of the message sending.
-     */
-    public function sendDocument(string $phone = '', string $url = '', string $caption = '', string $filename = '', $unique = false, $aggregate = false): mixed  {
-        return $this->sendMedia('document', $phone, $url, $caption, $filename, $unique, $aggregate);
+    public function sendDocument(string $phone = '', string $url = '', string $caption = '', $unique = false, $aggregate = false): mixed  {
+        return $this->sendMedia('document', $phone, $url, $caption, $unique, $aggregate);
     }
 
-    /**
-     * Send a media message (image or document).
-     * 
-     * @param string $type The type of media ('image' or 'document').
-     * @param string $phone The phone number.
-     * @param string $url The URL of the media.
-     * @param string $caption The caption for the media.
-     * @param string $filename The filename for the media.
-     * @param bool $unique Whether to make the message unique.
-     * @param bool $aggregate Whether to aggregate messages.
-     * @return mixed The result of the media message sending.
-     */
-    public function sendMedia(string $type = 'image', string $phone = '', string $url = '', string $caption = '', $filename = '', $unique = false, $aggregate = false): mixed {
+    public function sendMedia(string $type = 'image', string $phone = '', string $url = '', string $caption = '', $unique = false, $aggregate = false): mixed {
         if (empty($url) || empty($phone)) {
             $this->addInvalidMedia($type, $phone, $url, $caption, 'Url and phone number are required');
 
@@ -179,7 +106,7 @@ class OneSender {
             return $aggregate ? $this : [false, 'Invalid url'];
         } 
 
-        if ($this->validateUrl && $type === 'image' && $this->isUrlImage($url) === false) {
+        if ($this->validateUrl && $type == 'image' && $this->isUrlImage($url) !== true) {
             $this->addInvalidImage($phone, $url, $caption, 'Invalid image url');
             return $aggregate ? $this : [false, 'Invalid image url'];
         }
@@ -192,7 +119,7 @@ class OneSender {
             return $aggregate ? $this : [false, 'Invalid phone number'];
         }
 
-        $messages = $this->map($phones, function($phone) use ($type, $url, $caption, $filename, $unique) {
+        $messages = $this->map($phones, function($phone) use ($type, $url, $caption, $unique) {
 
             $msg = [
                 'link' => $url,
@@ -202,11 +129,8 @@ class OneSender {
                 $msg['caption'] = $caption;
             }
 
-            if (!empty($filename)) {
-                $msg['filename'] = $filename;
-            }
-
             $message = [
+                'app' => $this->app,
                 'to' => $phone['number'],
                 'recipient_type' => $phone['type'],
                 'type' => $type,
@@ -229,22 +153,27 @@ class OneSender {
         return $this->deliver($messages);
     }
 
-    /**
-     * Deliver messages to the API.
-     * 
-     * @param array $messages Array of messages to be delivered.
-     * @return array The result of the delivery.
-     */
+
     public function deliver(array $messages): array {
+
+        $messages = $this->applyFilters('messages', $messages);
+        $this->messages = $messages;
+
+        $apiUrl = $this->apiUrl;
+        $apiKey = $this->apiKey;
+
+        $apiUrl = $this->applyFilters('api_url', $apiUrl);
+        $apiKey = $this->applyFilters('api_key', $apiKey);
+
         $headers = array(
-            'Authorization: Bearer ' . $this->apiKey,
+            'Authorization: Bearer ' . $apiKey,
             'Content-Type: application/json'
         );
 
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => $this->apiUrl,
+            CURLOPT_URL => $apiUrl,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_ENCODING => '',
             CURLOPT_MAXREDIRS => 10,
@@ -277,21 +206,10 @@ class OneSender {
         return [$result, $error];
     }
 
-    /**
-     * Send all aggregated messages.
-     * 
-     * @return array The result of sending aggregated messages.
-     */
     public function send(): array {
         return $this->deliver($this->messages);
     }
 
-    /**
-     * Parse phone numbers and categorize them.
-     * 
-     * @param string $phone The input phone numbers.
-     * @return array Array of parsed and categorized phone numbers.
-     */
     private function parsePhones(string $phone): array {
         $phones = [];
         $input = explode(',', $phone);
@@ -320,14 +238,11 @@ class OneSender {
 
         $phones = $this->filter($phones, fn($item) => $item !== false);
 
+        $phones = $this->applyFilters('recipients', $phones);
+
         return $phones;
     }
 
-    /**
-     * Generate a random tag for unique messages.
-     * 
-     * @return string The generated random tag.
-     */
     private function randomTag(): string {
         return sprintf(
 			'%04x%04x%04x%04x%04x%04x%04x%04x',
@@ -342,40 +257,20 @@ class OneSender {
         );
     }
 
-    /**
-     * Map a callback function over an array.
-     * 
-     * @param array $items The input array.
-     * @param callable $callback The callback function.
-     * @return array The result of mapping the callback over the array.
-     */
     private function map(array $items, callable $callback ): array {
         return array_map( $callback, $items, array_keys( $items ) );
     }
 
-    /**
-     * Filter an array based on a callback function.
-     * 
-     * @param array $items The input array.
-     * @param callable $callback The callback function.
-     * @return array The result of filtering the array.
-     */
     private function filter(array $items, callable $callback ) {
         return array_filter( $items, $callback, ARRAY_FILTER_USE_BOTH );
     }
 
-    /**
-     * Check if a URL points to an image.
-     * 
-     * @param string $url The URL to check.
-     * @return bool True if the URL is an image, false otherwise.
-     */
     private function isUrlImage(string $url): bool {
 
         $curl = curl_init();
 
         curl_setopt_array($curl, array(
-            CURLOPT_URL => $url,
+            CURLOPT_URL => $this->apiUrl,
             CURLOPT_RETURNTRANSFER => true,
             CURLOPT_HEADER => true,
             CURLOPT_NOBODY => true,
@@ -407,14 +302,6 @@ class OneSender {
         return in_array($matches[1], ['image/jpeg', 'image/png']);
     }
 
-    /**
-     * Add an invalid text message to the list of invalid messages.
-     * 
-     * @param string $phone The phone number.
-     * @param string $text The text of the message.
-     * @param string $error The error message for the invalid text.
-     * @return void
-     */
     private function addInvalidText(string $phone, string $text, string $error): void {
         $invalidMessage = [
             'type' => 'text',
@@ -427,16 +314,7 @@ class OneSender {
         ];
         $this->invalidMessages = array_merge($this->invalidMessages, [$invalidMessage]);
     }
-
-    /**
-     * Add an invalid image message to the list of invalid messages.
-     * 
-     * @param string $phone The phone number.
-     * @param string $url The URL of the image.
-     * @param string $caption The caption for the image.
-     * @param string $error The error message for the invalid image.
-     * @return void
-     */    
+    
     private function addInvalidImage(string $phone, string $url, string $caption, string $error): void {
         $invalidMessage = [
             'type' => 'image',
@@ -451,15 +329,6 @@ class OneSender {
         $this->invalidMessages = array_merge($this->invalidMessages, [$invalidMessage]);
     }
 
-    /**
-     * Add an invalid document message to the list of invalid messages.
-     * 
-     * @param string $phone The phone number.
-     * @param string $url The URL of the document.
-     * @param string $caption The caption for the document.
-     * @param string $error The error message for the invalid document.
-     * @return void
-     */
     private function addInvalidDocument(string $phone, string $url, string $caption, string $error): void {
         $invalidMessage = [
             'type' => 'document',
@@ -474,16 +343,6 @@ class OneSender {
         $this->invalidMessages = array_merge($this->invalidMessages, [$invalidMessage]);
     }
 
-    /**
-     * Add an invalid media message (image or document) to the list of invalid messages.
-     * 
-     * @param string $type The type of media ('image' or 'document').
-     * @param string $phone The phone number.
-     * @param string $url The URL of the media.
-     * @param string $caption The caption for the media.
-     * @param string $error The error message for the invalid media.
-     * @return void
-     */
     private function addInvalidMedia(string $type, string $phone, string $url, string $caption, string $error): void {
         if ($type === 'image') {
             $this->addInvalidImage($phone, $url, $caption, $error);
@@ -492,22 +351,14 @@ class OneSender {
         }
     }
 
-    /**
-     * Get invalid messages.
-     * 
-     * @return array Array of invalid messages.
-     */
+    public function getMessages(): array {
+        return $this->messages;
+    }
+
     public function getInvalidMessages(): array {
         return $this->invalidMessages;
     }
 
-    /**
-     * Parses a template string by replacing placeholders with corresponding values from the given data array.
-     *
-     * @param string $template The template string containing placeholders in the format '{placeholder}'.
-     * @param array $data An associative array where keys are placeholders and values are the replacements.
-     * @return string The parsed template string with replaced placeholders.
-     */
     public static function parseTemplate(string $template = '', array $data = []): string {
         return self::parse_template($template, $data);
     }
@@ -523,5 +374,30 @@ class OneSender {
         }
 
         return $template;
+    }
+
+    public function addFilter(string $hook, $callback, int $priority) {
+        if (!isset($this->filters[$hook])) {
+            $this->filters[$hook] = array();
+        }
+        $this->filters[$hook][] = array(
+            'callback' => $callback,
+            'priority' => $priority
+        );
+    }
+
+    public function applyFilters($hook, ...$args) {
+        if (isset($this->filters[$hook])) {
+            usort($this->filters[$hook], function ($a, $b) {
+                return $a['priority'] - $b['priority'];
+            });
+
+            foreach ($this->filters[$hook] as $filter) {
+                $callback = $filter['callback'];
+                $args[0] = call_user_func_array($callback, $args);
+            }
+        }
+
+        return $args[0] ?? null;
     }
 }
